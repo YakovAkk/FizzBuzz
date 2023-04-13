2a45de5e79736dc1fc4a2c31dc5097476f0ba664
Yakov <Yakiv@DESKTOP-EL7FVD8>
2023-04-13 17:23:47 +0300

    fizzbazz modelu was added

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/YakovAkk/FizzBuzz/fizzbuzz"
+	"github.com/YakovAkk/FizzBuzz/rules"
+)
 
 func main() {
-	fmt.Println("A")
+	var usersNum = 10
+
+	rule1 := rules.Rule{Word: "Fizz", Number: 3}
+	rule2 := rules.Rule{Word: "Buzz", Number: 5}
+
+	fizzBuzz := fizzbuzz.FizzBuzz{Rules: []rules.Rule{rule1, rule2}}
+
+	for i := 1; i <= usersNum; i++ {
+		fmt.Print(fizzBuzz.apply(i))
+	}
 }
